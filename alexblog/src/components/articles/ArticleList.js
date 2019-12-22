93c7136201d09fe7fb302dc58696971381588aef
Preda Alexandru <alexpreda95@yahoo.com>
2019-12-22 20:31:58 +0200

    Create Article Summary

--- alexblog/src/components/articles/ArticleList.js
+++ alexblog/src/components/articles/ArticleList.js
@@ -1,33 +1,13 @@
 import React from 'react'
+import ArticleSummary from './ArticleSummary'
 
 const ArticleList = () => {
   return (
-    <div className="project-list section">
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title ">Article title</span>
-          <p>Posted by Alex</p>
-          <p className="grey-text">3rd September, 2am</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title ">Article title</span>
-          <p>Posted by Alex</p>
-          <p className="grey-text">3rd September, 2am</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title ">Article title</span>
-          <p>Posted by Alex</p>
-          <p className="grey-text">3rd September, 2am</p>
-        </div>
-      </div>
-      
+    <div className="project-list section">  
+      <ArticleSummary />
+      <ArticleSummary />
+      <ArticleSummary />
+      <ArticleSummary />
     </div>
   )
 }
