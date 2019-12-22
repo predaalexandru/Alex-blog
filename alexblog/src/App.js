50feefe24bba1dee00573ce5bfa812cc5d1f2072
Preda Alexandru <alexpreda95@yahoo.com>
2019-12-22 10:03:41 +0200

    Setting up and creating empty components

---
+++ alexblog/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>Alex Blog</h1>
+      </div>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
