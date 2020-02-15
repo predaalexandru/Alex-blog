21063b66772ddf8dfaee82005598a30fc06a1d1c
Preda Alexandru <alexpreda95@yahoo.com>
2020-02-15 16:30:05 +0200

    Adding data to firestore

--- alexblog/src/store/actions/articleActions.js
+++ alexblog/src/store/actions/articleActions.js
@@ -1,7 +1,18 @@
 export const createArticle = (article) => {
     // dispatch - dispatches an action to a reducers
     return (dispatch, getState, {getFirebase, getFirestore}) => {
-        dispatch({type: 'CREATE_ARTICLE', article});
-    }
+        const firestore = getFirestore();
+        firestore.collection('articles').add({
+            ...article,
+            authorFirstName: 'Alex',
+            authorLastName: 'Preda',
+            authorId: 12345,
+            createdAt: new Date()
+          }).then(() => {
+            dispatch({ type: 'CREATE_ARTICLE_SUCCESS' });
+          }).catch(err => {
+            dispatch({ type: 'CREATE_ARTICLE_ERROR' }, err);
+          });
+        }
         
 };
--- alexblog/src/store/reducers/articleReducer.js
+++ alexblog/src/store/reducers/articleReducer.js
@@ -9,10 +9,15 @@ const initState = {
 
 const articleReducer = (state = initState, action) => {
   switch (action.type) {
-    case 'CREATE_ARTICLE':
-      console.log('created article', action.article);
+    case 'CREATE_ARTICLE_SUCCESS':
+      console.log('Created article success');
+      return state;
+    case 'CREATE_ARTICLE_ERROR':
+      console.log('Created article error');
+      return state;
+    default:
+      return state;
   }
-  return state
 };
 
 export default articleReducer
