f8a5d02fc4c3798e239d66a2345f50850b702799
Preda Alexandru <alexpreda95@yahoo.com>
2020-03-07 21:08:28 +0200

    Track status login<->logout

--- alexblog/src/components/layout/Navbar.js
+++ alexblog/src/components/layout/Navbar.js
@@ -7,13 +7,12 @@ import {connect} from 'react-redux'
 
 const Navbar = (props) => {
     const { auth } = props;
-    console.log(auth);
+    const links = auth.uid ? <SignedIn /> : <SignedOut />;
     return (
         <nav className="nav-wrapper grey darken-3"> 
          <div className="container">
          <Link to='/' className="brand-logo">Alex Blog</Link>
-         <SignedIn />
-         <SignedOut />
+         {links}
          </div>
         </nav>
     )
--- alexblog/src/index.js
+++ alexblog/src/index.js
@@ -11,6 +11,8 @@ import { reduxFirestore, getFirestore, createFirestoreInstance } from 'redux-fir
 import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
 import Firebase from './config/Firebase'
 import firebase from 'firebase/app'
+import { useSelector  } from 'react-redux'
+import { isLoaded  } from 'react-redux-firebase';
 
 
 const store = createStore(rootReducer,
@@ -27,14 +29,14 @@ const rrfProps = {
     createFirestoreInstance
   };
 
-  ReactDOM.render(
-    <Provider store={store}>
-      <ReactReduxFirebaseProvider {...rrfProps}>
-        <App />
-      </ReactReduxFirebaseProvider>
-    </Provider>,
-    document.getElementById("root")
-  );
+  function AuthIsLoaded({ children }) {
+    const auth = useSelector(state => state.firebase.auth)
+    if (!isLoaded(auth)) return <div>Loading Screen...</div>;
+        return children
+}
+
+ReactDOM.render(<Provider store={store}> <ReactReduxFirebaseProvider {...rrfProps}> <AuthIsLoaded><App /> </AuthIsLoaded></ReactReduxFirebaseProvider></Provider>, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
